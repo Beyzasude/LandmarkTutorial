63d0cfdf377d28a7d63097efff22fa2df3951c49
Beyza Sude <erolbeyzasude@gmail.com>
2023-04-06 16:22:10 +0300

    Initial Commit

---
+++ LandmarkTutorial/LandmarkTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkTutorialApp.swift
+//  LandmarkTutorial
+//
+//  Created by Beyza Sude Erol on 6.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
